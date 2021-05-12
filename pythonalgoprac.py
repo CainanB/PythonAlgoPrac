294f29241cd9305fac9a4e37cca09f41712f4a38
Cainan Barboza <CainanBarboza1@gmail.com>
2021-05-12 02:47:11 -0500

    python recursion practice

--- pythonalgoprac.py
+++ pythonalgoprac.py
@@ -21,20 +21,55 @@
 # then gcd(a,b) = gcd(b,r)
 
 
-def gcd(a, b):
-  assert int(a) == a and int(b) == b, 'The numbers must be integers only'
-  if a < 0:
-    a = -1 * a
-  if b < 0:
-    b = -1 * b
-  if a == 0:
-    return b
-  if b == 0:
-    return a
-  #q = a // b
-  r = a % b
+# def gcd(a, b):
+#   assert int(a) == a and int(b) == b, 'The numbers must be integers only'
+#   if a < 0:
+#     a = -1 * a
+#   if b < 0:
+#     b = -1 * b
+#   if a == 0:
+#     return b
+#   if b == 0:
+#     return a
+#   #q = a // b
+#   r = a % b
 
-  return gcd(b,r)
+#   return gcd(b,r)
 
-print(gcd(288,16))
-print(gcd(16,288))
+# print(gcd(288,16))
+# print(gcd(16,288))
+
+
+# My solution to converting decimal to binary
+# def dec_to_binary(num, binary_code=[]):
+#   assert int(num) == num, 'The numbers must be integers only'
+#   quotient = int(num / 2)
+#   remainder = num % 2
+#   binary_code.insert(0,str(remainder))
+#   if quotient == 0:
+#     return "".join(binary_code)
+#   return dec_to_binary(quotient, binary_code)
+
+# print(dec_to_binary(123456789))
+
+# #Video solution for converting decimal to binary
+
+# def decimal_to_binary(n):
+#   assert int(n) == n, 'The parameter must be an integer only'
+#   if n == 0:
+#     return 0
+#   return n%2 + 10 * decimal_to_binary(int(n/2))
+
+# print(decimal_to_binary(10))
+
+# Factorial Recursion Practice
+
+def factorial(num):
+    if num == 0 or num == 1:
+        return 1
+    return num * factorial(num - 1)
+
+print(factorial(4))
+print(factorial(5))
+print(factorial(6))
+print(factorial(7))
